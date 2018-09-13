c7038a16f104585bbfb6cf607762df88086bcb3f
Howard Butler <howard@hobu.co>
2018-09-13 11:34:00 -0500

    int, not str

--- batch/info/runner/enqueue.py
+++ batch/info/runner/enqueue.py
@@ -31,7 +31,7 @@ name = key.split('.')[0].split('/')[-1]
 key = 's3://' + bucket + '/'+ key
 print (key, name)
 overrides = {
-	"memory": "%d"%MEMORY,
+	"memory": MEMORY,
 	'command': ['%s'%key]
     }
 

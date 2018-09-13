fa4662acf1d26e7a9a3bc1fab3b5fb3684fd7f01
Howard Butler <howard@hobu.co>
2018-09-13 10:41:39 -0500

    overwrite check

--- batch/info/runner/info.py
+++ batch/info/runner/info.py
@@ -5,10 +5,19 @@ import subprocess
 import json
 import os
 import boto3
+from botocore.errorfactory import ClientError
 import sys
 
 bucket = 'usgs-lidar'
 s3_client = boto3.client('s3')
+
+
+OVERWRITE=False
+try:
+    sys.argv[2]
+    OVERWRITE=True
+except:
+    pass
 
 paginator = s3_client.get_paginator( "list_objects" )
 page_iterator = paginator.paginate(Bucket=bucket,
@@ -34,7 +43,19 @@ def get_keys(s3_client, bucket, prefix=''):
             keys.append(k['Key'])
     return keys
 
+def checkExists(key):
+    bucket = 'usgs-lidar-pdal-metadata'
 
+    try:
+        s3_client.head_object(bucket, key)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            return False
+        else:
+            # Something else has gone wrong.
+            raise
+    return True
 
 folders = get_folders(s3_client, bucket, prefix='Projects/%s/' % (sys.argv[1]))
 
@@ -44,12 +65,21 @@ for folder in folders:
 
         command = ['python','enqueue.py', key ]
 	print command
-        p = subprocess.Popen(command,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if (err):
-            print (err)
+
+        EXISTS = checkExists(key+'.json')
+        WRITE = (not EXISTS)
+        if OVERWRITE and EXISTS:
+            WRITE = True
+
+        if WRITE:
+            p = subprocess.Popen(command,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            if (err):
+                print (err)
+        else:
+            print('key exists and not overwriting')
 	#sys.exit()
 
 

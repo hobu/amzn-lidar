a5dcf4de6f8b5d904c415ceeb0a494aa19d75900
Howard Butler <howard@hobu.co>
2018-09-25 13:32:56 -0500

    r53 internal

--- userdata/r53-register.sh
+++ userdata/r53-register.sh
@@ -45,7 +45,7 @@ cat > ${TMPADDS} << EOF
                 {
                         "Action": "UPSERT",
                         "ResourceRecordSet": {
-                                "Name": "$DNS_INTERNAL.$ZONE",
+                                "Name": "$DNS_INTERNAL-internal.$ZONE",
                                 "Type": "A",
                                 "TTL": 60,
                                 "ResourceRecords": [{

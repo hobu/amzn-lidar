1a071d8dc39b65d858a4c414f28f97a1e8ea530c
Howard Butler <howard@hobu.co>
2018-10-29 15:00:38 -0500

    fix init

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ source env.sh
 docker pull 275986415235.dkr.ecr.us-west-2.amazonaws.com/info
 #/bin/bash ./userdata/r53-register.sh
 cd info/runner
-python3 dequeue.py >>/mnt/deqlog.log 2>&1
+python3 dequeue.py >>/mnt/pdalinfo/deqlog.log 2>&1
